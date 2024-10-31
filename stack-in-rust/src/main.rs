9238304d854a9db0867512b60a884941e71ef12c
Karan0207 <minidox0207@gmail.com>
2024-10-31 23:05:20 +0530

    stack in rust

---
+++ stack-in-rust/src/main.rs
@@ -0,0 +1,25 @@
+struct Stack<T> {
+    elements: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { elements: Vec::new() }
+    }
+
+    fn push(&mut self, item: T) {
+        self.elements.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.elements.pop()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.elements.last()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.elements.is_empty()
+    }
+}
