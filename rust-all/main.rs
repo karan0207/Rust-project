0b9bbb37e50263ad5f88858b8aa12a2f46633fd1
Karan0207 <minidox0207@gmail.com>
2024-11-13 23:25:45 +0530

    TaskStatus added

--- rust-all/main.rs
+++ rust-all/main.rs
@@ -3,3 +3,9 @@ use std::io::{self, Write, BufReader, BufRead};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
+
+// Enum to represent the status of the task
+enum TaskStatus {
+    Pending,
+    Completed,
+}
