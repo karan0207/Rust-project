0b7f28e65a923cd7bba26503f6867946d8a8105f
Karan0207 <minidox0207@gmail.com>
2024-11-14 23:25:16 +0530

    entry point added in contract instantiate func

--- blockchain-infrastructure/contracts/hello-contract/src/contracts.rs
+++ blockchain-infrastructure/contracts/hello-contract/src/contracts.rs
@@ -2,3 +2,16 @@ use cosmwasm_std::{Deps, DepsMut, Env, MessageInfo, Response, StdResult, entry_p
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
 
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    let state = State {
+        message: msg.message,
+    };
+    STATE.save(deps.storage, &state)?;
+    Ok(Response::default())
+}
