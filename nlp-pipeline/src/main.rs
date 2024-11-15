03937baa19e1d1b461535ade29057a2875d101f1
Karan0207 <minidox0207@gmail.com>
2024-11-15 23:08:18 +0530

    checking for device

--- nlp-pipeline/src/main.rs
+++ nlp-pipeline/src/main.rs
@@ -8,6 +8,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 // Load a pre-trained tokenizer (e.g., GPT-2)
 let tokenizer = Tokenizer::from_pretrained("gpt2", None)?;
 
-
+ // Set up the device (use GPU if available)
+ let device = if tch::Cuda::is_available() {
+    Device::Cuda(0)
+} else {
+    Device::Cpu
+};
 
 }
