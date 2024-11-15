ad946f975b90095fd55bdb1dea6184d9525f03ba
Karan0207 <minidox0207@gmail.com>
2024-11-15 23:07:51 +0530

    tokenizer with gpt2

--- nlp-pipeline/src/main.rs
+++ nlp-pipeline/src/main.rs
@@ -1,2 +1,13 @@
 use tch::{nn, Device, Tensor};
 use tokenizers::{Tokenizer, PaddingParams, TruncationParams};
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+// Load a pre-trained tokenizer (e.g., GPT-2)
+let tokenizer = Tokenizer::from_pretrained("gpt2", None)?;
+
+
+
+}
