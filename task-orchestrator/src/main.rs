77fea8db46f0e2dfaff2924dbbb949f03cb0fba8
Karan0207 <minidox0207@gmail.com>
2024-11-17 23:27:17 +0530

    task interface created

--- task-orchestrator/src/main.rs
+++ task-orchestrator/src/main.rs
@@ -4,3 +4,10 @@ use std::fs;
 use tokio::process::Command as TokioCommand;
 use tracing::{info, Level};
 use tracing_subscriber;
+
+#[derive(Debug, Deserialize)]
+struct Task {
+    name: String,
+    command: String,
+    depends_on: Option<Vec<String>>,
+}
