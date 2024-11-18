e66f8c1a434485a02f84119651d151e330019291
Karan0207 <minidox0207@gmail.com>
2024-11-18 23:08:45 +0530

    interface for ui

--- rust-smartdoc/frontend/src/main.rs
+++ rust-smartdoc/frontend/src/main.rs
@@ -2,3 +2,15 @@ use yew::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use serde::{Deserialize, Serialize};
 use gloo_net::http::Request;
+
+
+#[derive(Serialize)]
+struct DocumentationRequest {
+    function_name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct DocumentationResponse {
+    description: String,
+    example: String,
+}
