915dfdee68eb9d6c140cfcba3ec0717a833b7d08
Karan0207 <minidox0207@gmail.com>
2024-11-18 23:05:54 +0530

    interface created for request and response

--- rust-smartdoc/src/main.rs
+++ rust-smartdoc/src/main.rs
@@ -1,2 +1,13 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct DocumentationRequest {
+    function_name: String,
+}
+
+#[derive(Serialize)]
+struct DocumentationResponse {
+    description: String,
+    example: String,
+}
