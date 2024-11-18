9d2638c0153d0ec621a8015c6c699ab4185cd635
Karan0207 <minidox0207@gmail.com>
2024-11-18 23:06:23 +0530

    function for get documentation

--- rust-smartdoc/src/main.rs
+++ rust-smartdoc/src/main.rs
@@ -11,3 +11,18 @@ struct DocumentationResponse {
     description: String,
     example: String,
 }
+
+async fn get_documentation(info: web::Json<DocumentationRequest>) -> impl Responder {
+    let function_name = &info.function_name;
+
+    // Simulated response (you can use rustdoc data here)
+    let response = DocumentationResponse {
+        description: format!("Detailed explanation of function `{}`", function_name),
+        example: format!(
+            "Example usage of `{}`:\n\nfn main() {{\n    println!(\"Hello, {}!\");\n}}",
+            function_name, function_name
+        ),
+    };
+
+    HttpResponse::Ok().json(response)
+}
