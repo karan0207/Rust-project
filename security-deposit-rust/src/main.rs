0abf917554aeecc3c2423feb6b927d3753c35ee7
Karan0207 <minidox0207@gmail.com>
2024-11-19 23:20:32 +0530

    implementation of security deposit func

--- security-deposit-rust/src/main.rs
+++ security-deposit-rust/src/main.rs
@@ -8,3 +8,15 @@ struct SecurityDeposit {
     deposit_date: String,
     deductions: f64,
 }
+
+
+impl SecurityDeposit {
+    // Create a new deposit
+    fn new(tenant_name: &str, amount: f64, deposit_date: &str) -> SecurityDeposit {
+        SecurityDeposit {
+            tenant_name: tenant_name.to_string(),
+            amount,
+            deposit_date: deposit_date.to_string(),
+            deductions: 0.0,
+        }
+    }
