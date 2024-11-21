f6fedf61acc05aba16702a0ece1c1d534722581c
Karan0207 <minidox0207@gmail.com>
2024-11-21 23:12:42 +0530

    interface created for stock-data

--- rusty-trader/src/main.rs
+++ rusty-trader/src/main.rs
@@ -1,3 +1,14 @@
 use std::error::Error;
 use std::fs::File;
 use csv::ReaderBuilder;
+
+
+#[derive(Debug, serde::Deserialize)]
+struct StockData {
+    Date: String,
+    Open: f64,
+    High: f64,
+    Low: f64,
+    Close: f64,
+    Volume: u64,
+}
